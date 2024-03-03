e92f1dc2bbfdeb6c72788d1e4894befc486cf984
Cyril Jacquet <cyril.jacquet@skribisto.eu>
2024-03-03 21:52:56 +0100

    fix: adaptation for being an example

--- examples/simple/src/gui/qml_application/realqmlmodules.cmake
+++ examples/simple/src/gui/qml_application/realqmlmodules.cmake
@@ -3,7 +3,7 @@
 
 add_subdirectory(real_imports)
 
-target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE
+target_link_libraries(${APP_NAME} PRIVATE
 simple-example-qml-interactorsplugin
 simple-example-qml-modelsplugin
 simple-example-qml-singlesplugin
