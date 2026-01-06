ca5941bc594c1b8e9c5970b3ff64f28ce20d9f29
Cyril Jacquet <cyril.jacquet@mailfence.com>
2026-01-06 10:39:02 +0100

    chore: remove obsolete devcontainer and test files
    - Deleted `.devcontainer` configuration files, test database utilities, and associated headers to clean up unused code.
    - Removed auto-generated files from `tests/database/common` to reflect project restructuring and deprecation of legacy components.

--- tests/database/contracts/repository/signal_holder.cpp
+++
@@ -1,9 +0,0 @@
-// This file was generated automatically by Qleany's generator, edit at your own risk! 
-// If you do, be careful to not overwrite it when you run the generator again.
-#include "repository/signal_holder.h"
-
-using namespace DatabaseTest::Contracts::Repository;
-
-SignalHolder::SignalHolder(QObject *parent) : QObject{parent}
-{
-}
