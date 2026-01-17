680982d483344eea3b878995f40280f5ce436366
Cyril Jacquet <cyril.jacquet@mailfence.com>
2026-01-17 17:42:21 +0100

    chore(examples): remove old qleany examples

--- examples/front_ends/src/core/contracts/database/types.h
+++
@@ -1,9 +0,0 @@
-// This file was generated automatically by Qleany's generator, edit at your own risk! 
-// If you do, be careful to not overwrite it when you run the generator again.
-#pragma once
-#include <QString>
-
-namespace FrontEnds::Contracts::Database
-{
-using SaveData = QMap<QString, QList<QVariantHash>>;
-} // namespace FrontEnds::Contracts::Database
--- examples/simple/src/core/contracts/repository/signal_holder.cpp
+++
@@ -1,9 +0,0 @@
-// This file was generated automatically by Qleany's generator, edit at your own risk! 
-// If you do, be careful to not overwrite it when you run the generator again.
-#include "repository/signal_holder.h"
-
-using namespace Simple::Contracts::Repository;
-
-SignalHolder::SignalHolder(QObject *parent) : QObject{parent}
-{
-}
